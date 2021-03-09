15ed7121a35f7f8d5cb57262e0e02a8016d5deeb
Larry Joy <ljoy@microsoft.com>
2021-03-08 19:10:43 -0800

    Add full fhir-bundle schema; add fhir definitions file; update schema tests

--- src/schema.ts
+++ src/schema.ts
@@ -5,63 +5,23 @@ import fs from 'fs';
 import path from 'path';
 import Log from './logger';
 import { ErrorCode } from './error';
-
-
+import metaSchema from 'ajv/lib/refs/json-schema-draft-06.json';
+import fhirSchema from '../schema/fhir-definitions-schema.json';
+import Ajv, { AnySchemaObject } from "ajv";
 // http://json-schema.org/
 // https://github.com/ajv-validator/ajv
-import JsonValidator, { AnySchemaObject } from "ajv";
 
 
-export async function validateFromFile(schemaPath: string, data: FhirBundle | JWS | JWSPayload | HealthCard, log: Log): Promise<boolean> {
-
-    if (!fs.existsSync(schemaPath)) {
-        log.fatal('Schema file not found : ' + schemaPath, ErrorCode.SCHEMA_ERROR);
-        return false;
-    }
-
-    const schemaDir = path.basename(path.dirname(schemaPath));
-
-    // for each $ref in our schema, load the schema file into an object and return it.
-    const jsonValidator = new JsonValidator({
-        allErrors: true,
-        loadSchema: (uri) => {
-            const schemaFile: string = uri.slice(uri.lastIndexOf('/'));
-            const schemaFullPath = path.join(schemaDir, schemaFile);
-            const schema = JSON.parse(fs.readFileSync(schemaFullPath, 'utf8')) as AnySchemaObject;
-            return Promise.resolve(schema);
-        }
-    });
-
-    const schemaObj = JSON.parse(fs.readFileSync(schemaPath, 'utf8')) as AnySchemaObject;
-
-    return jsonValidator.compileAsync(schemaObj)
-        .then(validate => {
-            if (validate(data)) { return true; }
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            validate.errors!.forEach(ve => {
-                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-                log.error('Schema: ' + ve.schemaPath + '  \'' + ve.message + '\'', ErrorCode.SCHEMA_ERROR);
-            });
-            return false;
-        })
-        .catch(err => {
-            // TODO: get to this catch in test
-            log.error('Schema: ' + (err as Error).message, ErrorCode.SCHEMA_ERROR);
-            return false;
-        });
-
-}
-
-
-export function validateSchema(schema: AnySchemaObject, data: FhirBundle | JWS | JWSPayload | HealthCard, log: Log): boolean {
+export function validateSchema(schema: AnySchemaObject | AnySchemaObject[], data: FhirBundle | JWS | JWSPayload | HealthCard, log: Log): boolean {
 
     // by default, the validator will stop at the first failure. 'allErrors' allows it to keep going.
-    const jsonValidator = new JsonValidator({ allErrors: true });
+    const ajv = new Ajv({ allErrors: true, strict: false });
+    ajv.addMetaSchema(metaSchema);  // required for avj 7 to support json-schema draft-06 (draft-07 is current)
 
     try {
 
         // TODO: make this fail in test
-        const validate = jsonValidator.compile(schema);
+        const validate = ajv.addSchema(fhirSchema).compile(schema);
 
         if (validate(data)) { return true; }
 
--- tests/schema.test.ts
+++ tests/schema.test.ts
@@ -1,14 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { AnySchemaObject } from 'ajv';
 import fs from 'fs';
 import path from 'path';
-import Log, { LogLevels } from '../src/logger';
-import { validateFromFile } from '../src/schema';
+import Log from '../src/logger';
+import { validateSchema } from '../src/schema';
 
 
 const schemaDir = './schema';
 const exampleDir = './testdata';
+
 
 // Map our schema files to the examples. The tests below can lookup 
 // the appropriate schema with the 'lookup' function
@@ -43,45 +45,39 @@ const lookup = function (example: string): string {
     throw new Error('Example not found : ' + example);
 };
 
-const examples: string[] = [
-    'issuer.jwks.public.json',
-    'example-00-a-fhirBundle.json',
-    'example-00-b-jws-payload-expanded.json',
-    'example-00-c-jws-payload-minified.json',
-    'example-00-d-jws.txt',
-    'example-00-e-file.smart-health-card',
-    'example-01-a-fhirBundle.json',
-    'example-01-b-jws-payload-expanded.json',
-    'example-01-c-jws-payload-minified.json',
-    'example-01-d-jws.txt',
-    'example-01-e-file.smart-health-card'
-];
+
+const log = new Log('Schema Tests');
 
 
-examples.forEach(exampleName => {
-    testSchema(exampleName);
-});
-
-
-
-function testSchema(exampleFile: string): void {
-    // TODO: the next logical step would be to read each example file out of the examples directory
-    //       and determine the appropriate schema and then build a test.
-    //       this would require code that can determine schema by analysing the example.
+function testSchema(exampleFile: string): boolean  {
 
     const schemaName = lookup(exampleFile);
     const schemaPath = path.resolve(schemaDir, schemaName + ".json");
+    const schema = JSON.parse(fs.readFileSync(schemaPath, 'utf-8')) as AnySchemaObject;
+
     const examplePath = path.resolve(exampleDir, exampleFile);
     const fileData = fs.readFileSync(examplePath, 'utf-8');
     const ext = path.extname(examplePath);
     const dataObj = ext !== '.txt' ? JSON.parse(fileData) as FhirBundle | JWS | JWSPayload | HealthCard : fileData;
-    const log = new Log('testSchema');
 
-    test("Schema: " + schemaName + " " + exampleFile, async () => {
-        const result = await validateFromFile(schemaPath, dataObj, log);
-        expect(result).toBe(true);
-        expect(log.flatten(LogLevels.WARNING).length).toBe(0);
-    });
+    const result = validateSchema(schema, dataObj, log);
 
-    return;
+    expect(result).toBe(true);
+
+    return result;
 }
+
+
+test("Schema: valid 00-a-fhirBundle", () => { testSchema('example-00-a-fhirBundle.json'); });
+test("Schema: valid 00-b-jws-payload-expanded", () => { testSchema('example-00-b-jws-payload-expanded.json'); });
+test("Schema: valid 00-c-jws-payload-minified", () => { testSchema('example-00-c-jws-payload-minified.json'); });
+test("Schema: valid 00-d-jws", () => { testSchema('example-00-d-jws.txt'); });
+test("Schema: valid 00-e-file.smart-health-card", () => { testSchema('example-00-e-file.smart-health-card'); });
+
+test("Schema: valid issuer.jwks.public", () => { testSchema('issuer.jwks.public.json'); });
+
+test("Schema: valid 01-a-fhirBundle", () => { testSchema('example-01-a-fhirBundle.json'); });
+test("Schema: valid 01-b-jws-payload-expanded", () => { testSchema('example-01-b-jws-payload-expanded.json'); });
+test("Schema: valid 01-c-jws-payload-minified", () => { testSchema('example-01-c-jws-payload-minified.json'); });
+test("Schema: valid 01-d-jws", () => { testSchema('example-01-d-jws.txt'); });
+test("Schema: valid 01-e-file.smart-health-card", () => { testSchema('example-01-e-file.smart-health-card'); });
