d8bcb9d857534c8742c92dc6bea798a9af3b5a73
Christian Paquin <cpaquin@microsoft.com>
2021-04-16 07:05:04 -0400

    Removed TODO comment

--- tests/card.test.ts
+++ tests/card.test.ts
@@ -216,7 +216,6 @@ test("Cards: valid 00 FHIR bundle with non-dm properties", testCard(['test-examp
 
 test("Cards: valid 00 FHIR bundle with non-short refs", testCard(['test-example-00-a-short-refs.json'], "fhirbundle", [0, 4 /*4x ErrorCode.SCHEMA_ERROR*/]));
 
-// TODO: der examples need to be generated to avoid schema errors introduced by spec v0.4.5
 test("Cards: der encoded signature", testCard(['test-example-00-d-jws-der-signature.txt'], 'jws', [[ErrorCode.SIGNATURE_FORMAT_ERROR]]));
 
 test("Cards: der encoded signature s-negative", testCard(['test-example-00-d-jws-der-signature-s-neg.txt'], 'jws', [[ErrorCode.SIGNATURE_FORMAT_ERROR]]));
