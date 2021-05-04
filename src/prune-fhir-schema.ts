83660be938c4202620c5c2c3c0bdb8fbeaec1779
Larry Joy <ljoy@microsoft.com>
2021-05-04 13:28:05 -0700

    Download zipped fhir schema on demand; removed Extensions from schema to further shrink;

--- src/prune-fhir-schema.ts
+++ src/prune-fhir-schema.ts
@@ -1,19 +1,54 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import fhir_schema from '../schema/fhir-schema-full.json';
+//import fhir_schema from '../schema/fhir-schema-full.json';
 import fs from 'fs';
+import JSZip from 'jszip'
+import got from 'got'
+
+
+const fullSchemaLink = 'https://hl7.org/fhir/fhir.schema.json.zip'
+
+
+//
+// Unzip the full FHIR schema
+//
+async function getFullSchema(): Promise<Schema> {
+
+    const zip = new JSZip();
+    const zipPath = "./schema/fhir.schema.json.zip";
+    const outPath = './schema/fhir-schema-full.json';
+    const zipFileName = "fhir.schema.json";
+
+    const zipBuffer = await got(fullSchemaLink).buffer();
+
+    // process the buffer with zip
+    const zipArchive = await zip.loadAsync(zipBuffer);
+
+    // extract a selected file - there would be only one file for our use
+    const zipFileData = zipArchive.file(zipFileName);
+    if (!zipFileData) throw new Error(zipFileName + " not found in zip file : " + zipPath);
+
+    // get the un-zipped data
+    const fileContent = await zipFileData.async('string');
+    fs.writeFileSync(outPath, fileContent);
+
+    return JSON.parse(fileContent) as Schema;
+}
+
 
 
 //
 // Creates a smaller FHIR sub-schema from a list of FHIR Resources
 // The new schema will contain the Resources and any child definitions required by those Resources.
 //
-function pruneSchema(resources: string[], fullFhirSchema: Schema): Schema {
+async function pruneSchema(resources: string[]/*, fullFhirSchema?: Schema*/): Promise<Schema> {
+
+    const fullFhirSchema: Schema = await getFullSchema();
 
     const newSchema: Schema = {
-        $schema: fullFhirSchema.$schema,
-        $id: fullFhirSchema.$id,
+        $schema: "http://json-schema.org/draft-07/schema#",
+        $id: "https://smarthealth.cards/schema/fhir-schema.json",
         oneOf: [{
             $ref: "#/definitions/Bundle"
         }],
@@ -44,32 +79,62 @@ function pruneSchema(resources: string[], fullFhirSchema: Schema): Schema {
         }
     })
 
+    // Schema validation of the Bundle.entries will happen separately.  We'll replace the ResourceList type
+    // with a generic object to prevent further validation here.
+    // The reason is that if the entry items have bad schema, we will get dozens of errors as the bad-schema object
+    // fails to match any of the possible Resources. So instead, we validate the entries individually against
+    // the resource type specified in its resourceType field.
+    newSchema.definitions['Bundle_Entry'].properties!.resource = { "type": "object" };
+
     return newSchema;
 }
 
-function findChildRefs(schema: Schema, definitionName: string, definitionList: string[]): void {
+function findChildRefs(schema: Schema, definitionName: string, definitionList: string[], indent = ""): void {
 
     // Special case, do nothing as we are in the process of building this list
-    if(definitionName === 'ResourceList') return;
+    if (definitionName === 'ResourceList') return;
 
     // do nothing is this is a known def
     if (definitionList.includes(definitionName)) return;
 
+    console.log(indent + definitionName);
+
     // add the definition
     const definition = schema.definitions[definitionName];
+    delete definition.description;
+
     definitionList.push(definitionName);
 
     // recurse the defs properties
     for (const key in definition.properties) {
+
+        // Extension properties begin with '_' - remove them unless extensions are required
+        if (key.substring(0, 1) === '_') {
+            delete definition.properties[key];
+            continue;
+        }
+
         const prop = definition.properties[key];
+        // remove description field to save some space
+        delete prop.description;
+
+
         if (prop["$ref"]) {
             const ref = prop["$ref"].slice(prop["$ref"].lastIndexOf('/') + 1);
-            findChildRefs(schema, ref, definitionList);
+            definitionList.includes(ref) || findChildRefs(schema, ref, definitionList, indent + "    ");
             continue;
         }
         if (prop.items && '$ref' in prop.items) {
             const ref = prop.items.$ref.slice(prop.items["$ref"].lastIndexOf('/') + 1);
-            findChildRefs(schema, ref, definitionList);
+
+            // remove Extension properties to further reduce the schema size
+            // comment this out is Extensions are required
+            if (ref === 'Extension') {
+                delete definition.properties[key];
+                continue;
+            }
+
+            definitionList.includes(ref) || findChildRefs(schema, ref, definitionList, indent + "    ");
             continue;
         }
     }
@@ -77,7 +142,7 @@ function findChildRefs(schema: Schema, definitionName: string, definitionList: s
     return;
 }
 
-const prunedFhirSchema = pruneSchema([
+void pruneSchema([
     "Bundle",
     "Patient",
     "DiagnosticReport",
@@ -91,6 +156,7 @@ const prunedFhirSchema = pruneSchema([
     "MedicationRequest",
     "Medication",
     "Specimen"
-], fhir_schema as Schema);
+]).then(schema => {
+    fs.writeFileSync('./schema/fhir-schema.json', JSON.stringify(schema, null, 2));
+})
 
-fs.writeFileSync('./schema/fhir-schema.json', JSON.stringify(prunedFhirSchema, null, 2));
