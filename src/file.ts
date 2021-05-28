e24ed02b04df7edb4986697adcd7f53633612f4b
Christian Paquin <cpaquin@microsoft.com>
2021-05-28 08:22:54 -0400

    Added support for node 16 by removing dependency on blocking istextorbinary package.

--- src/file.ts
+++ src/file.ts
@@ -3,7 +3,6 @@
 
 import fs from 'fs';
 import path from 'path';
-import { isText, getEncoding } from 'istextorbinary';
 import fileType from 'file-type';
 import core from 'file-type/core';
 import { PNG } from 'pngjs';
@@ -18,8 +17,6 @@ export interface FileInfo {
     name: string,
     path: string,
     ext: string,
-    encoding: string | null,
-    type: "text" | "binary",
     buffer: Buffer,
     fileType: core.FileTypeResult | string | undefined,
     image?: { data: Buffer, height: number, width: number } | undefined
@@ -40,44 +37,36 @@ export async function getFileData(filepath: string): Promise<FileInfo> {
         name: path.basename(filepath),
         path: path.resolve(filepath),
         ext: path.extname(filepath),
-        type: isText(filepath, buffer) ? "text" : "binary",
-        encoding: getEncoding(buffer),
         buffer: buffer,
         fileType: await fileType.fromBuffer(buffer)
     };
 
-    if (fileInfo.type === 'text') {
-
-        const result: FileData = {
-            data: buffer.toString('utf-8').trim(),
-            type: "unknown"
-        };
-
-        if ((result.data as string).startsWith("<svg")) {
-            fileInfo.fileType = 'svg';
+    if (fileInfo.fileType) {
+        if ((fileInfo.fileType as core.FileTypeResult).ext) {
+            fileInfo.fileType = (fileInfo.fileType as core.FileTypeResult).ext;
         }
-
-        if ((result.data as string).startsWith("shc:")) {
+    } else {
+        // let's try to determine the type, assuming text files
+        const textFileContent = buffer.toString('utf-8');
+        if (fileInfo.ext === '.svg' || textFileContent.startsWith("<svg")) {
+            fileInfo.fileType = 'svg';
+        } else if (textFileContent.startsWith("shc:")) {
             fileInfo.fileType = 'shc';
         }
+    }
 
-    } else {
-        fileInfo.fileType = (fileInfo.fileType as core.FileTypeResult).ext;
-
-        switch (fileInfo.fileType) {
-            case 'png':
-                fileInfo.image = PNG.sync.read(fileInfo.buffer);
-                break;
-            case 'jpg':
-                fileInfo.image = jpeg.decode(buffer, { useTArray: true }) as { data: Buffer, height: number, width: number };
-                break;
-            case 'bmp':
-                fileInfo.image = decode(buffer) as { data: Buffer, height: number, width: number };
-                break;
-            default:
-                break;
-        }
-
+    switch (fileInfo.fileType) {
+        case 'png':
+            fileInfo.image = PNG.sync.read(fileInfo.buffer);
+            break;
+        case 'jpg':
+            fileInfo.image = jpeg.decode(buffer, { useTArray: true }) as { data: Buffer, height: number, width: number };
+            break;
+        case 'bmp':
+            fileInfo.image = decode(buffer) as { data: Buffer, height: number, width: number };
+            break;
+        default:
+            break;
     }
 
     return fileInfo;
