9b4c95692845e05d29c254ebc8bd0472b2b5470c
ljoy913 <ljoy@microsoft.com>
2022-01-12 21:36:02 -0800

    fix check-for-updates typescript failure

--- src/check-for-update.ts
+++ src/check-for-update.ts
@@ -6,8 +6,8 @@ import semver from 'semver';
 
 export async function latestDevToolsVersion(): Promise<string | undefined> {
     try {
-        const packageJson = await got('https://raw.githubusercontent.com/smart-on-fhir/health-cards-dev-tools/main/package.json').json();
-        if (!packageJson || !packageJson.version) return;
+        const packageJson : {version: string} = await got('https://raw.githubusercontent.com/smart-on-fhir/health-cards-dev-tools/main/package.json').json();
+        if (!packageJson?.version) return;
         const v = semver.valid(packageJson.version);
         return v ? v : undefined;
     } catch {
