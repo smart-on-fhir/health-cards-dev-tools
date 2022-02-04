a9ac630978248d1de9de0d53aecfbf08672bf69e
ljoy913 <ljoy@microsoft.com>
2022-02-04 10:19:09 -0800

    fix option checking issue

--- src/options.ts
+++ src/options.ts
@@ -9,7 +9,7 @@ interface IOptions {
     cascade: boolean,
     logOutputPath: string,
     skipJwksDownload: boolean,
-    jwkset : string,
+    jwkset: string,
     validator: Validators
 }
 
@@ -21,7 +21,7 @@ const defaultOptions: IOptions = {
     cascade: true,
     logOutputPath: '',
     skipJwksDownload: false,
-    jwkset : '',
+    jwkset: '',
     validator: Validators.default
 }
 
@@ -33,15 +33,16 @@ const setOptions = function (options: Partial<IOptions> = {}): IOptions {
     });
 
     if (options) {
-        if ('logLevel' in options && (!options.logLevel || !(options.logLevel in LogLevels))) throw new Error(`Invalid logLevel ${options.logLevel ?? ''}`);
-        if ('profile' in options && (!options.profile || !(options.profile in ValidationProfiles))) throw new Error(`Invalid profile ${options.profile ?? ''}`);
+        /* Apparently TS does not consider 'isInteger()' as typeguard so we need the ' ?? -n' to coerce not-really-undefined to an invalid integer*/
+        if ('logLevel' in options && (!Number.isInteger(options.logLevel) || !((options.logLevel ?? -2) in LogLevels))) throw new Error(`Invalid logLevel ${options.logLevel ?? ''}`);
+        if ('profile' in options && (!Number.isInteger(options.profile)  || !((options.profile ?? -1)in ValidationProfiles))) throw new Error(`Invalid profile ${options.profile ?? ''}`);
         if ('issuerDirectory' in options && typeof options.issuerDirectory !== 'string') throw new Error(`Invalid issuerDirectory ${options.issuerDirectory ?? ''}`);
         if ('clearKeyStore' in options && typeof options.clearKeyStore !== 'boolean') throw new Error(`Invalid clearKeyStore ${options.clearKeyStore ?? ''}`);
         if ('cascade' in options && typeof options.cascade !== 'boolean') throw new Error(`Invalid cascade ${options.cascade ?? ''}`);
         if ('skipJwksDownload' in options && typeof options.skipJwksDownload !== 'boolean') throw new Error(`Invalid skipJwksDownload ${options.skipJwksDownload ?? ''}`);
         if ('logOutputPath' in options && typeof options.logOutputPath !== 'string') throw new Error(`Invalid logOutputPath ${options.logOutputPath ?? ''}`);
         if ('jwkset' in options && typeof options.jwkset !== 'string') throw new Error(`Invalid jwkset ${options.jwkset ?? ''}`);
-        if ('validator' in options && (!options.validator || !(options.validator in Validators))) throw new Error(`Invalid validator ${options.validator ?? ''}`);
+        if ('validator' in options && (!Number.isInteger(options.validator) || !((options.validator ?? -1) in Validators))) throw new Error(`Invalid validator ${options.validator ?? ''}`);
     }
 
     return { // this syntax merges two object into a single object. For common properties, the second entry wins.
