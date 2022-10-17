3c7b3a948bd23dcb1584de401b6736990d978cbd
Larry Joy <ljoy@microsoft.com>
2022-10-17 16:07:48 -0700

    single jar download

--- src/fhirValidator.ts
+++ src/fhirValidator.ts
@@ -1,37 +1,40 @@
-import fs from 'fs';
-import path from 'path';
-import Log from '../src/logger';
-import { ErrorCode } from './error';
-import color from 'colors';
-import got from 'got';
-import { runCommand, runCommandSync } from '../src/command';
-import crypto from 'crypto';
+import fs from "fs";
+import path from "path";
+import Log from "../src/logger";
+import { ErrorCode } from "./error";
+import color from "colors";
+import got from "got";
+import { runCommand, runCommandSync } from "../src/command";
+import crypto from "crypto";
 
-const imageName = 'fhir.validator.image';
-const dockerFile = 'fhir.validator.Dockerfile';
-const dockerContainer = 'fhir.validator.container';
-const validatorJarFile = 'validator_cli.jar';
-const validatorUrl = 'https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar';
-const tempFilePrefix = 'tempfhirbundle';
-
+const imageName = "fhir.validator.image";
+const dockerFile = "fhir.validator.Dockerfile";
+const dockerContainer = "fhir.validator.container";
+const validatorJarFile = "validator_cli.jar";
+const validatorUrl = "https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar";
+const tempFilePrefix = "tempfhirbundle";
 
 // share a log between the functions. This can be passed in externally through the validate() function
 let log: Log;
 
-
 async function downloadFHIRValidator(): Promise<void> {
+
+    if(fs.existsSync(validatorJarFile)) return Promise.resolve();
+
     try {
-        fs.writeFileSync(validatorJarFile, (await got(validatorUrl, { followRedirect: true }).buffer()));
+        const buffer = await got(validatorUrl, { followRedirect: true }).buffer();
+        fs.writeFileSync(validatorJarFile, buffer);
+        const stats = fs.statSync(validatorJarFile);
+        log.debug(`Stats:\n${JSON.stringify(stats, null, 2)}`);
     } catch (err) {
         log.debug(`File download error ${(err as Error).toString()}`);
     }
 }
 
-
 // Runs the FHIR validator using the installed JRE
 async function runValidatorJRE(artifactPath: string): Promise<CommandResult | null> {
-
-    if (!fs.existsSync(validatorJarFile)) await downloadFHIRValidator();
+    //if (!fs.existsSync(validatorJarFile)) await downloadFHIRValidator();
+    await downloadFHIRValidator();
 
     if (!fs.existsSync(validatorJarFile)) {
         log.error(`Failed to download FHIR Validator Jar file ${validatorJarFile} from ${validatorUrl}`);
@@ -41,42 +44,44 @@ async function runValidatorJRE(artifactPath: string): Promise<CommandResult | nu
     //const tempJarFile = `validator_cli_${crypto.randomBytes(4).readUInt32LE(0)}.jar`;
     const tempJarFile = `validator_cli.jar`;
 
-    fs.copyFileSync(`./${validatorJarFile}`, tempJarFile);
+    //fs.copyFileSync(`./${validatorJarFile}`, tempJarFile);
 
-    const result: CommandResult = await runCommand(`java -jar ./${tempJarFile} -jurisdiction US ./${artifactPath}`, `Running HL7 FHIR validator with JRE`, log);
+    const result: CommandResult = await runCommand(
+        `java -jar ./${tempJarFile} -jurisdiction US ./${artifactPath}`,
+        `Running HL7 FHIR validator with JRE`,
+        log
+    );
 
-    fs.rmSync(tempJarFile);
+    //fs.rmSync(tempJarFile);
 
     return result;
 }
 
-
 // Runs the FHIR validator in a Docker container
 async function runValidatorDocker(artifactPath: string): Promise<CommandResult | null> {
+    if (!(await Docker.checkPermissions())) return null;
 
-    if (!await Docker.checkPermissions()) return null;
-
-    if (!await Docker.imageExists(imageName)) {
+    if (!(await Docker.imageExists(imageName))) {
         log.debug(`Image ${imageName} not found. Attempting to build.`);
-        if (!await Docker.buildImage(dockerFile, imageName)) {
-            log.error('Could not build Docker image.');
+        if (!(await Docker.buildImage(dockerFile, imageName))) {
+            log.error("Could not build Docker image.");
             return null;
         }
     }
 
     const dockerCommand = `java -jar validator_cli.jar data/${artifactPath}`;
 
-    // create a new container from image, copies the 
-    const command = `docker run --rm --name ${dockerContainer} -v ${path.resolve(path.dirname(artifactPath))}:/data  ${imageName} ${dockerCommand}`;
+    // create a new container from image, copies the
+    const command = `docker run --rm --name ${dockerContainer} -v ${path.resolve(
+        path.dirname(artifactPath)
+    )}:/data  ${imageName} ${dockerCommand}`;
 
     const result = await runCommand(command, `Running HL7 FHIR validator with Docker (${imageName})`, log);
 
     return result;
 }
 
-
-export async function validate(fileOrJSON: string, logger = new Log('FHIR Validator')): Promise<Log> {
-
+export async function validate(fileOrJSON: string, logger = new Log("FHIR Validator")): Promise<Log> {
     log = logger;
 
     const usingJre = JRE.isAvailable();
@@ -85,14 +90,16 @@ export async function validate(fileOrJSON: string, logger = new Log('FHIR Valida
 
     if (!usingJre && !usingDocker) {
         return log.error(
-            `Validator: use of option ${color.italic('--validator fhirvalidator')} requires Docker or JRE to execute the FHIR Validator Java application.  See: http://hl7.org/fhir/validator/`,
+            `Validator: use of option ${color.italic(
+                "--validator fhirvalidator"
+            )} requires Docker or JRE to execute the FHIR Validator Java application.  See: http://hl7.org/fhir/validator/`,
             ErrorCode.JRE_OR_DOCKER_NOT_AVAILABLE
         );
     }
 
     if (JSON.parse(fileOrJSON)) {
         log.debug(`writing valid json as temp file ${tempFileName}`);
-        fs.writeFileSync(tempFileName, fileOrJSON);  // overwrites by default
+        fs.writeFileSync(tempFileName, fileOrJSON); // overwrites by default
         fileOrJSON = tempFileName;
     } else {
         log.debug(`not valid JSON ${fileOrJSON}, no temp file created.`);
@@ -120,13 +127,13 @@ export async function validate(fileOrJSON: string, logger = new Log('FHIR Valida
     if (result && /Information: All OK/.test(result?.stdout)) return log;
 
     const errors = result?.stdout.match(/(?<=\n\s*Error @ ).+/g) || [];
-    errors.forEach(err => {
+    errors.forEach((err) => {
         const formattedError = err; // splitLines(err);
         log.error(formattedError, ErrorCode.FHIR_VALIDATOR_ERROR);
     });
 
     const warnings = result?.stdout.match(/(?<=\n\s*Warning @ ).+/g) || [];
-    warnings.forEach(warn => {
+    warnings.forEach((warn) => {
         const formattedError = warn; // splitLines(warn);
         log.warn(formattedError, ErrorCode.FHIR_VALIDATOR_ERROR);
     });
@@ -140,40 +147,39 @@ export async function validate(fileOrJSON: string, logger = new Log('FHIR Valida
     return log;
 }
 
-
 const JRE = {
-
     isAvailable: (): boolean => {
         const result = runCommandSync(`java -version`, log);
         if (result.exitCode === 0) {
-            const version = /^(java \d+.+|openjdk version "\d+.+)/.exec(result.stdout || result.stderr)?.[0] ?? 'unknown';
+            const version =
+                /^(java \d+.+|openjdk version "\d+.+)/.exec(result.stdout || result.stderr)?.[0] ?? "unknown";
             log?.debug(`Java detected : ${version}`);
         }
 
         return result.exitCode === 0;
-    }
-
-}
-
+    },
+};
 
 const Docker = {
-
     isAvailable: (): boolean => {
         const result = runCommandSync(`docker --version`, log);
         if (result.exitCode === 0) {
-            const version = /^Docker version \d+.+/.exec(result.stdout)?.[0] ?? 'unknown';
+            const version = /^Docker version \d+.+/.exec(result.stdout)?.[0] ?? "unknown";
             log?.debug(`Docker detected : ${version}`);
         }
         return result.exitCode === 0;
     },
 
     imageExists: async (imageName: string): Promise<boolean> => {
-        return (await runCommand(`docker image inspect ${imageName}`, `Check Docker image ${imageName} exists`, log)).exitCode === 0;
+        return (
+            (await runCommand(`docker image inspect ${imageName}`, `Check Docker image ${imageName} exists`, log))
+                .exitCode === 0
+        );
     },
 
     containerExists: async (name: string): Promise<boolean> => {
         const stdout = (await runCommand(`docker ps -a --format '{{.Names}}'`, undefined, log)).stdout;
-        const names: string[] = stdout.replace(/'/g, '').split('\n');
+        const names: string[] = stdout.replace(/'/g, "").split("\n");
         return names.includes(name);
     },
 
@@ -191,10 +197,7 @@ const Docker = {
                     ErrorCode.DOCKER_DAEMON_NOT_RUNNING
                 );
             } else {
-                log.error(
-                    `Docker command failed ${result.stderr}`,
-                    ErrorCode.DOCKER_ERROR
-                );
+                log.error(`Docker command failed ${result.stderr}`, ErrorCode.DOCKER_ERROR);
             }
         }
         return result.exitCode === 0;
@@ -206,7 +209,6 @@ const Docker = {
     },
 
     buildImage: async (dockerFile: string, imageName: string): Promise<boolean> => {
-
         if (!fs.existsSync(dockerFile)) {
             log.error(`Cannot find Dockerfile ${dockerFile}`);
             return false;
@@ -214,9 +216,13 @@ const Docker = {
 
         log.debug(`Building Docker image ${imageName} from ${dockerFile}`);
 
-        const result = await runCommand(`docker build -t ${imageName} -f ${dockerFile} .`, `Build Docker image ${imageName} from ${dockerFile}`, log);
+        const result = await runCommand(
+            `docker build -t ${imageName} -f ${dockerFile} .`,
+            `Build Docker image ${imageName} from ${dockerFile}`,
+            log
+        );
 
-        if (result.exitCode === 0 && await Docker.imageExists(imageName)) {
+        if (result.exitCode === 0 && (await Docker.imageExists(imageName))) {
             log.debug(`Docker image ${imageName} created.`);
         } else {
             log.debug(`Failed to build image ${imageName}`);
@@ -227,10 +233,8 @@ const Docker = {
         log.debug(result.stdout || result.stderr);
 
         return result.exitCode === 0;
-    }
-
-}
-
+    },
+};
 
 export function jreOrDockerAvailable(): boolean {
     return JRE.isAvailable() || Docker.isAvailable();
